03efab73ad9726f4f57f5b2bf14a9e89df10193f
Jonathan Maw <jonathan.maw@gmail.com>
2015-03-04 18:54:52 +0000

    Implement pingpong on server and client

--- client.c
+++ client.c
@@ -1,3 +1,99 @@
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "pingpong-client-protocol.h"
+
+struct context
 {
+    struct wl_registry *registry;
+    struct wl_display *display;
+    struct ping_pong *ping_pong;
+    int stop;
+};
+
+static void
+handle_pong(void *data, struct ping_pong *ping_pong, const char *message)
+{
+    struct context *ctx = data;
+    printf("Received pong, message='%s'\n", message);
+    ctx->stop = 1;
+}
+
+static const struct ping_pong_listener ping_pong_listener = {
+    handle_pong
+};
+
+static void
+registry_handle_global_remove(void *data, struct wl_registry *registry,
+                              uint32_t name)
+{
+    /* Nobody cares about globals being removed */
+}
+
+static void
+registry_handle_global(void *data, struct wl_registry *registry, uint32_t name,
+                       const char *interface, uint32_t version)
+{
+    struct context *ctx = data;
+    if (strcmp(interface, "ping_pong") == 0)
+    {
+        ctx->ping_pong = wl_registry_bind(registry, name, &ping_pong_interface, 1);
+        if (!ctx->ping_pong)
+        {
+            fprintf(stderr, "Failed to registry bind ping_pong");
+            return;
+        }
+        ping_pong_add_listener(ctx->ping_pong, &ping_pong_listener, ctx);
+    }
+}
+
+static const struct wl_registry_listener registry_client_listener = {
+    registry_handle_global,
+    registry_handle_global_remove
+};
+
+int main(int argc, char *argv[])
+{
+    struct context ctx = {0};
+    
+    if (argc <= 1)
+    {
+        fprintf(stderr, "Not enough arguments. Usage: PROG MESSAGE\n");
+        return EXIT_FAILURE;
+    }
+
+    ctx.display = wl_display_connect(NULL);
+    if (!ctx.display)
+    {
+        fprintf(stderr, "Failed to get display\n");
+        return EXIT_FAILURE;
+    }
+
+    ctx.registry = wl_display_get_registry(ctx.display);
+    if (!ctx.registry)
+    {
+        fprintf(stderr, "Failed to get registry\n");
+        return EXIT_FAILURE;
+    }
+
+    if (wl_registry_add_listener(ctx.registry, &registry_client_listener, &ctx))
+    {
+        fprintf(stderr, "Failed to add registry listener\n");
+        return EXIT_FAILURE;
+    }
+
+    wl_display_roundtrip(ctx.display);
+
+    if (ctx.ping_pong == NULL)
+    {
+        fprintf(stderr, "ping pong hasn't been bound\n");
+        return EXIT_FAILURE;
+    }
+    
+    printf("Sending ping with message %s\n", argv[1]);
+    ping_pong_ping(ctx.ping_pong, argv[1]);
+
+    while(!ctx.stop)
+        wl_display_dispatch(ctx.display);
+
 }
--- server.c
+++ server.c
@@ -1,7 +1,81 @@
+#include <stdlib.h>
+
 #include <weston/compositor.h>
+
+#include "pingpong-server-protocol.h"
+
+struct context
+{
+    struct wl_list list_clients;
+};
+
+struct client_context
+{
+    struct wl_list link;
+    struct wl_resource *ping_pong_interface_resource;
+    struct context *context;
+};
+
+static void
+ping_pong_ping(struct wl_client *client, struct wl_resource *resource, const char *message)
+{
+    weston_log("Received ping %s\n", message);
+    ping_pong_send_pong(resource, message);
+}
+
+static const struct ping_pong_interface ping_pong_implementation = {
+    ping_pong_ping
+};
+
+static void
+unbind_ping_pong(struct wl_resource *resource)
+{
+    struct client_context *client_ctx = wl_resource_get_user_data(resource);
+    wl_list_remove(&client_ctx->link);
+    free(client_ctx);
+}
+
+static void
+bind_ping_pong(struct wl_client *client, void *data, uint32_t version, uint32_t id)
+{
+    struct context *ctx = data;
+    struct client_context *client_ctx;
+    client_ctx = calloc(1, sizeof *client_ctx);
+    if (!client_ctx)
+    {
+        weston_log("No memory to allocate client context\n");
+        return;
+    }
+
+    client_ctx->ping_pong_interface_resource =
+        wl_resource_create(client, &ping_pong_interface, 1, id);
+    wl_resource_set_implementation(client_ctx->ping_pong_interface_resource,
+                                   &ping_pong_implementation, client_ctx,
+                                   unbind_ping_pong);
+    client_ctx->context = ctx;
+    wl_list_init(&client_ctx->link);
+    wl_list_insert(&ctx->list_clients, &client_ctx->link);
+}
 
 WL_EXPORT int
 module_init(struct weston_compositor *compositor, int *argc, char *argv[])
 {
+    struct context *ctx;
     weston_log("Pingpong module initialized!");
+    
+    ctx = calloc(1, sizeof *ctx);
+    if (!ctx)
+    {
+        weston_log("No memory ot initialize context\n");
+        return -1;
+    }
+
+    wl_list_init(&ctx->list_clients);
+
+    if (wl_global_create(compositor->wl_display, &ping_pong_interface, 1,
+                         ctx, bind_ping_pong) == NULL)
+    {
+        return -1;
+    }
+
 }
