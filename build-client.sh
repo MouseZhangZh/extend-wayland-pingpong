d45fe9750f163651cad0c69aa9657fcf1ffb3e5d
MouseZhang <sendbypython@foxmail.com>
2022-09-13 07:33:49 +0000

    Fix build client shell.

--- build-client.sh
+++ build-client.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-LIBS="$(pkg-config --libs wayland-client)"
-CFLAGS="$(pkg-config --cflags wayland-client)"
-gcc $LIBS $CFLAGS -o client client.c pingpong-protocol.c
+gcc $LIBS $CFLAGS -o client client.c pingpong-protocol.c -lwayland-client
